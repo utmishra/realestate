a0d435fccc572ac70493887fe6c66a1ed0b4465a
Utkarsh Mishra <utmishra@gmail.com>
2017-01-24 02:07:34 +0530

    Fixed auto drop of schema on application stop

--- src/main/java/com/realestate/config/RealEstateConfig.java
+++ src/main/java/com/realestate/config/RealEstateConfig.java
@@ -61,7 +61,7 @@ public class RealEstateConfig {
  
    Properties additionalProperties() {
       Properties properties = new Properties();
-      properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+      properties.setProperty("hibernate.hbm2ddl.auto", "update");
       properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
       return properties;
    }
