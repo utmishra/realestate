02f35c32653b1025d32a12ce905806779def628e
Utkarsh Mishra <utmishra@gmail.com>
2017-02-06 10:10:38 +0530

    Added sample SQL indexing script

---
+++ listing_types.sql
@@ -0,0 +1,144 @@
+INSERT INTO `realestate`.`attribute_types`
+(`id`,
+`attribute_name`,
+`attribute_title`,
+`attribute_values`,
+`selection_type`,
+`listing_type`)
+VALUES
+(1,
+'number_of_rooms',
+'Number of Rooms',
+'',
+4,
+2);
+
+INSERT INTO `realestate`.`attribute_types`
+(`id`,
+`attribute_name`,
+`attribute_title`,
+`attribute_values`,
+`selection_type`,
+`listing_type`)
+VALUES
+(2,
+'usable_surface',
+'Usable surface',
+'',
+4,
+2);
+
+INSERT INTO `realestate`.`attribute_types`
+(`id`,
+`attribute_name`,
+`attribute_title`,
+`attribute_values`,
+`selection_type`,
+`listing_type`)
+VALUES
+(3,
+'partitioning',
+'Partitioning',
+'{"Decomandat", "Semidecomandat", "Nedecomandat", "Circular"}',
+3,
+2);
+
+INSERT INTO `realestate`.`attribute_types`
+(`id`,
+`attribute_name`,
+`attribute_title`,
+`attribute_values`,
+`selection_type`,
+`listing_type`)
+VALUES
+(4,
+'floor',
+'Floor',
+'',
+4,
+2);
+
+INSERT INTO `realestate`.`attribute_types`
+(`id`,
+`attribute_name`,
+`attribute_title`,
+`attribute_values`,
+`selection_type`,
+`listing_type`)
+VALUES
+(5,
+'construction_year',
+'Construction year',
+'',
+4,
+2);
+INSERT INTO `realestate`.`attribute_types`
+(`id`,
+`attribute_name`,
+`attribute_title`,
+`attribute_values`,
+`selection_type`,
+`listing_type`)
+VALUES
+(6,
+'tipimobil',
+'Tipimobil',
+'{"Bloc", "Casa-Vila"}',
+3,
+2);
+INSERT INTO `realestate`.`attribute_types`
+(`id`,
+`attribute_name`,
+`attribute_title`,
+`attribute_values`,
+`selection_type`,
+`listing_type`)
+VALUES
+(7,
+'state',
+'State',
+'{"New", "Renovated", "Under Construction"}',
+3,
+2);
+INSERT INTO `realestate`.`attribute_types`
+(`id`,
+`attribute_name`,
+`attribute_title`,
+`attribute_values`,
+`selection_type`,
+`listing_type`)
+VALUES
+(8,
+'confort',
+'Confort',
+'',
+4,
+2);
+INSERT INTO `realestate`.`attribute_types`
+(`id`,
+`attribute_name`,
+`attribute_title`,
+`attribute_values`,
+`selection_type`,
+`listing_type`)
+VALUES
+(9,
+'number_of_bathrooms',
+'Number of Bathrooms',
+'',
+4,
+2);
+INSERT INTO `realestate`.`attribute_types`
+(`id`,
+`attribute_name`,
+`attribute_title`,
+`attribute_values`,
+`selection_type`,
+`listing_type`)
+VALUES
+(10,
+'parking',
+'Parking',
+'{ "Yes", "No" }',
+3,
+2);
